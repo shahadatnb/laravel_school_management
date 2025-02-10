89b4dc8301e96dd4951ebe2c2e498745fcaecb1f
Shahadat Hosain <shahadat80@gmail.com>
2025-02-10 23:31:09 +0600

    update

--- resources/views/admin/student/id_card/template1/back.blade.php
+++ resources/views/admin/student/id_card/template1/back.blade.php
@@ -1,6 +1,6 @@
 <div class="id-card-back text-center align-middle">
-    @php $address = "ID:{$student->reg_no} Class: {$student->semester? $student->semester->name : ''}" @endphp
-    {!! QrCode::size(80)->meCard($student->name,$address,$student->mobile,session('branch')['email']) !!} <br>
+    @php $semester = $student->semester? $student->semester->name : ''; $address = "ID:{$student->reg_no} Class: {$semester}" @endphp
+    {{-- {!! QrCode::size(80)->meCard($student->name,$address,$student->mobile,session('branch')['email']) !!} <br> --}}
     <p style="font-weight: bold; color: red">If you found return to</p>
     <p style="font-size: 4mm; line-height: 4mm">
         {{ session('branch')['name'] }}, {{ session('branch')['address'] }}, Phone: {{ session('branch')['contact'] }}<br>
