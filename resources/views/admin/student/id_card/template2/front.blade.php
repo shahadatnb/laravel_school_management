51fed8da4f67b7d0bb694b8a83968d920fb17a25
Shahadat Hosain <shahadat80@gmail.com>
2025-02-10 21:59:33 +0600

    update

--- resources/views/admin/student/id_card/template2/front.blade.php
+++ resources/views/admin/student/id_card/template2/front.blade.php
@@ -1,99 +1,133 @@
-<div class="id-card-front position-relative">
-    <div class="text-center id-card-logo">
-      <img width="50" src="{{asset('/upload/logo/'.session('branch')['logo'])}}" alt="">
-      <h3 class="inst-name">{{ session('branch')['name'] }}</h3>
-    </div>
-    <div class="id-card-title text-center">
-        <h4>Identity Card</h4>
-    </div>
-    <p class="text-center id-card-id">ID No: {{ $student->reg_no }}</p>
-    <div class="text-center student-photo">
-      <img width="80" src="{{asset('/storage/'.$student->photo)}}" alt="">
-    </div>
-    <div class="id-card-info">              
-      <div style="font-weight: bold" class="text-center">{{ $student->name }}</div>
-      <span>Class</span>: {{ $student->semester? $student->semester->name : '' }}<br>
-      <span>Section</span>: {{ $student->section? $student->section->section? $student->section->section->name : '' : '' }} <br>
-      <span>Class Roll</span>: {{ $student->class_roll }}
-    </div>
-    <div class="signature position-absolute">
-      <img src="{{asset('/upload/head_sign/'.session('branch')['head_sign'])}}" alt=""> <br>
-      <span>Principal</span>
-    </div>
+<div class="id-card-front">
+  <div class="front-header">
+      <div class="school-name">Podtasha Ideal Academy</div>
+      <div class="school-address">Rajshahi</div>
+      <div class="establishment">ESTD-2008</div>
   </div>
+
+  <div class="id-title">ID CARD</div>
+
+  <div class="content-wrapper">
+      <div class="photo-section"></div>
+      <div class="details-section">
+          <div class="detail-row">
+              <div class="value" style="font-size: 1.1em; font-weight: bold;">MD. JULFIKAR (ALI)</div>
+          </div>
+          
+          <div class="detail-row">
+              <div class="label">Student ID</div>
+              <div class="value">2024416</div>
+          </div>
+
+          <div class="detail-row">
+              <div class="label">Class</div>
+              <div class="value">X (Day)</div>
+          </div>
+
+          <div class="detail-row">
+              <div class="label">Roll/Section</div>
+              <div class="value">249 / E</div>
+          </div>
+
+          <div class="detail-row">
+              <div class="label">Group</div>
+              <div class="value">Science</div>
+          </div>
+      </div>
+  </div>
+
+  <div class="footer-line"></div>
+
+  <div class="blood-group">
+      <span class="label">Blood Group:</span>
+      <span class="value">B+</span>
+  </div>
+
+  <div class="principal-section">
+      <div class="value">01796-111682</div>
+      <div class="label" style="margin-top: 5px;">Principal</div>
+  </div>
+</div>
 <style type="text/css">
-    .id-card-front, .id-card-back {
-      width: 53.98mm;
-      height: 85.60mm;
-      border: 3px solid #dbef79;
-      margin: 10px;
-      padding: 5px;
-    }
-    
-    .id-card-logo img {
-      max-height: 12mm !important;
-      max-width: 100%;
-    }
-    
-    .id-card-title {
-      background: #df002d;
-      padding: 2px;
-      margin: .5mm 0 .5mm 0;
-    }
-    
-    .id-card-title h4 {
-      color: #fff;
-      font-family: arial;
-      margin: 0;
-      font-size: 5mm;
-    }
-    
-    .inst-name {
-      font-size: 4mm;
-      font-family: arial;
-      line-height: 4mm;
-      margin: 0;
-    }
-    
-    .id-card-id {
-      margin: 0;
-      line-height: 4mm;
-      font-size: 4mm;
-      margin-bottom: 1mm;
-    }
-    .student-photo {
-      height: 22mm;
-    }
-    .student-photo img {
-      border: 3px solid #635bcd;
-      max-height: 100%;
-    }
-    .id-card-info {
-      line-height: 3.8mm;
-      margin-top: 5px;
-      font-size: 4mm;
-    }
-    
-    .id-card-info span {
-      width: 18mm;
-      display: inline-block;
-    }
-    
-    .signature {
-      width: 50%;
-      right: 0;
-      bottom: 0;
-      text-align: center;
-    }
-    
-    .signature img {
-      max-width: 80%;
-      max-height: 6mm;
-      margin-bottom: -2.5mm;
-    }
-    
-    .signature span {
-      border-top: 1px solid #000;
-      font-size: 4mm;
-    }
+  .id-card-front, .id-card-back {
+    width: 53.98mm;
+    height: 85.60mm;
+    border: 3px solid #dbef79;
+    margin: 10px;
+    padding: 5px;
+  }
+  
+  .front-header {
+            text-align: center;
+            border-bottom: 2px solid #000;
+            padding-bottom: 10px;
+            margin-bottom: 15px;
+        }
+
+        .id-card-front  .school-name {
+            font-size: 1.4em;
+            font-weight: bold;
+            text-transform: uppercase;
+        }
+
+        .id-card-front  .school-address {
+            font-size: 0.9em;
+            margin: 3px 0;
+        }
+
+        .id-card-front  .establishment {
+            font-size: 0.8em;
+            color: #555;
+        }
+
+        .id-card-front  .id-title {
+            text-align: center;
+            font-size: 1.8em;
+            margin: 15px 0;
+            text-decoration: underline;
+        }
+
+        .id-card-front  .photo-section {
+            width: 100px;
+            height: 120px;
+            border: 2px solid #000;
+            background: #eee;
+        }
+
+        .id-card-front  .details-section {
+            flex: 1;
+        }
+
+        .id-card-front  .detail-row {
+            margin-bottom: 8px;
+        }
+
+        .id-card-front  .label {
+            font-size: 0.8em;
+            color: #555;
+        }
+
+        .id-card-front  .value {
+            font-size: 0.9em;
+            font-weight: 500;
+        }
+
+        .id-card-front  .principal-section {
+            position: absolute;
+            bottom: 20px;
+            right: 20px;
+            text-align: right;
+        }
+
+        .id-card-front  .footer-line {
+            border-top: 2px solid #000;
+            margin: 15px 0;
+        }
+
+        .id-card-front  .blood-group {
+            position: absolute;
+            bottom: 20px;
+            left: 20px;
+            font-size: 0.9em;
+        }
 </style>
